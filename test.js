5bbfba093815366d8be2c3d9fa0cb2bfca929516
Sam Mateosian <sam@bigroomstudios.com>
2016-04-29 17:35:39 -0400

    working on connecting to api websockets

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
+var config = require('./config');
 var alteredClient = require('./index');
+
+var Socket = require('primus.io').createSocket({ transformer: 'websockets' });
+
 
 
 alteredClient.login(function(err, data){
@@ -8,6 +12,24 @@ alteredClient.login(function(err, data){
     alteredClient.getUser({}, function(user){
 
         console.log('user', user);
-    })
+
+        // get socket instance
+        var socket = new Socket('wss://api.altered.io:443?token='+alteredClient.token);
+
+        socket.on('open', function(data) {
+
+            console.log('socket open', data);
+        });
+
+        socket.on('subscribe', function (data) {
+            console.log('subscribe', data); // => good morning
+        });
+
+        socket.send('subscribe', { type: 'user', id: user.id });
+
+
+    });
+
+
 
 });
